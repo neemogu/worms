8e70aef71739e01088e8a376048efa0ef2b1a614
Nikita Pogodaev <nik180101@gmail.com>
2021-12-18 14:29:22 +0700

    controller exception handler

--- WormsStrategyServer/Controllers/WormsStrategyController.cs
+++ WormsStrategyServer/Controllers/WormsStrategyController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using WormsAdvanced;
 using WormStrategyAPI;
 
 namespace WormsStrategyServer.Controllers {
@@ -18,15 +17,22 @@ namespace WormsStrategyServer.Controllers {
 
         [HttpPost("{wormName}/getAction"), Produces("application/json"), Consumes("application/json")]
         public WormActionDTO GetNextAction(string wormName, int step, int run, [FromBody] WormsWorldStateDTO worldState) {
-            _logger.Log(LogLevel.Information, "Requested action for step {}, run {}, worm {}, world: {}", step, run, wormName, JsonConvert.SerializeObject(worldState));
-            var strategy = new WormsOptimalStrategy(FoodDTOMapper.FromDTOToMap(worldState.Food));
-            var allWorms = WormDTOMapper.FromDTOListToEntityList(worldState.Worms);
-            var worm = allWorms.Find((w) => w.Name.Equals(wormName));
-            if (worm == null) {
+            try {
+                _logger.Log(LogLevel.Information, "Requested action for step {}, run {}, worm {}, world: {}", step, run,
+                    wormName, JsonConvert.SerializeObject(worldState));
+                var strategy = new WormsOptimalStrategy(FoodDTOMapper.FromDTOToMap(worldState.Food));
+                var allWorms = WormDTOMapper.FromDTOListToEntityList(worldState.Worms);
+                var worm = allWorms.Find((w) => w.Name.Equals(wormName));
+                if (worm == null) {
+                    StatusCode(400);
+                    return null;
+                }
+
+                return ActionDTOMapper.GetDTOFromEntity(strategy.NextAction(worm, allWorms, step, run));
+            } catch (Exception) {
                 StatusCode(400);
-                return new WormActionDTO();
+                return null;
             }
-            return ActionDTOMapper.GetDTOFromEntity(strategy.NextAction(worm, allWorms, step, run));
         }
     }
 }
